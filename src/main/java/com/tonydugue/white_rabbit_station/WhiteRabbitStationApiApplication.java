4cfa0c633493b31fd8522c64b8aa2026cefda03d
Tony Dugué <duguetony@gmail.com>
2024-12-31 18:04:52 +0100

    init spring boot project with dependencies

---
+++ src/main/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplication.java
@@ -0,0 +1,13 @@
+package com.tonydugue.white_rabbit_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteRabbitStationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteRabbitStationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonydugue.white_rabbit_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteRabbitStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
