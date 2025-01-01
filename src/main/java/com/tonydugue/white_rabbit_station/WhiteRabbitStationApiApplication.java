f23ec7886d5d3e3443bf47d1e75b75a4241066c8
Tony Dugué <duguetony@gmail.com>
2025-01-01 14:24:02 +0100

    feat(user): add User entity

--- src/main/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplication.java
+++ src/main/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplication.java
@@ -2,8 +2,10 @@ package com.tonydugue.white_rabbit_station;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WhiteRabbitStationApiApplication {
 
 	public static void main(String[] args) {
