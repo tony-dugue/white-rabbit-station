9f60754538bcbf8dba327c1fc3297cba619e494e
Tony Dugué <duguetony@gmail.com>
2025-01-01 18:24:45 +0100

    feat(config): implement the JwtFilter class

--- src/main/java/com/tonydugue/white_rabbit_station/config/SecurityConfig.java
+++ src/main/java/com/tonydugue/white_rabbit_station/config/SecurityConfig.java
@@ -20,7 +20,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-  private final JwtFilter jwtFilter;
+  private final JwtFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
 
   @Bean
@@ -47,7 +47,7 @@ public class SecurityConfig {
       )
       .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
       .authenticationProvider(authenticationProvider)
-      .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
