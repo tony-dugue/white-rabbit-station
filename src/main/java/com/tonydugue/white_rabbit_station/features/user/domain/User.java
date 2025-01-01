2c6ed40a9f9508d5501e0dad56e9e8331d331523
Tony Dugué <duguetony@gmail.com>
2025-01-01 14:44:57 +0100

    feat(auth): add Role entity and relation with User entity

--- src/main/java/com/tonydugue/white_rabbit_station/features/user/domain/User.java
+++ src/main/java/com/tonydugue/white_rabbit_station/features/user/domain/User.java
@@ -1,11 +1,13 @@
-package com.tonydugue.white_rabbit_station.user.domain;
+package com.tonydugue.white_rabbit_station.features.user.domain;
 
+import com.tonydugue.white_rabbit_station.features.auth.domain.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -43,6 +46,9 @@ public class User implements UserDetails, Principal {
 
   private boolean enabled;
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  private List<Role> roles;
+
   @CreatedDate
   @Column(nullable = false, updatable = false)
   private LocalDateTime createdDate;
@@ -58,7 +64,10 @@ public class User implements UserDetails, Principal {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
+    return this.roles
+            .stream()
+            .map(r -> new SimpleGrantedAuthority(r.getName()))
+            .collect(Collectors.toList());
   }
 
   @Override
