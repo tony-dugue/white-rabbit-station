26a338df53f514525ab49b2135cb4f4859b390e9
Tony Dugué <duguetony@gmail.com>
2025-01-02 02:23:58 +0100

    fix(auth): create role USER if not exist in bdd

--- src/main/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplication.java
+++ src/main/java/com/tonydugue/white_rabbit_station/WhiteRabbitStationApiApplication.java
@@ -1,7 +1,11 @@
 package com.tonydugue.white_rabbit_station;
 
+import com.tonydugue.white_rabbit_station.features.auth.domain.Role;
+import com.tonydugue.white_rabbit_station.features.auth.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,4 +18,12 @@ public class WhiteRabbitStationApiApplication {
 		SpringApplication.run(WhiteRabbitStationApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
